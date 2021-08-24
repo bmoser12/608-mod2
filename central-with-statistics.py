e8e7060c7c7393ed0fb1ffad985fde38298de9cd
bmoser12 <89146471+bmoser12@users.noreply.github.com>
2021-08-24 15:09:05 -0500

    Update central-with-statistics.py

--- central-with-statistics.py
+++ central-with-statistics.py
@@ -1,1 +1,8 @@
-
+import statistics
+grades = [47, 95, 88, 73, 88, 84]
+mean = statistics.mean(grades)
+mode = statistics.mode(grades)
+median = statistics.median(grades)
+print('The mean is:', mean)
+print('The mode is:', mode)
+print('The median is:', median)
