1e7b07c62b4981687e335e890b8aecb06e034e9a
bmoser12 <89146471+bmoser12@users.noreply.github.com>
2021-08-24 15:28:54 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -1,6 +1,6 @@
 grades = [47, 95, 88, 73, 88, 84]
 total = sum(grades)
-count = len(grades
+count = len(grades)
 average = sum(grades)/len(grades):
 print('The average of the grades is:', average)
 print('The number of grades is:',count)
