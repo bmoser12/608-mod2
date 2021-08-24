dcf59aee9b6dd6ddfa1ed18c6c0500e8e7ad1ac7
bmoser12 <89146471+bmoser12@users.noreply.github.com>
2021-08-24 14:55:42 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -1,4 +1,6 @@
 values = [47, 95, 88, 73, 88, 84]
-print ('sum is'sum(values))
-print ('count is'len(values))
-print ('average is' average = sum(values)/len(values))
+for values:
+  print ('sum is'sum(values))
+  print ('count is'len(values))
+  print ('average is' average = sum(values)/len(values))
+
