dabdf1f2a1816f5a3f39e22f29c5dca7430d92da
bmoser12 <89146471+bmoser12@users.noreply.github.com>
2021-08-24 15:22:30 -0500

    Delete central-native.py

--- central-native.py
+++
@@ -1,7 +0,0 @@
-grades = [47, 95, 88, 73, 88, 84]
-total = sum(grades)
-count = len(grades
-average = sum(grades)/len(grades):
-print('The average of the grades is:', average)
-print('The number of grades is:',count)
-print('The total of the grades is:',total)
