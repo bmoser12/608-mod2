88898f7f3ac6ecb5160d5ae6bd005eb6c6fca28b
bmoser12 <89146471+bmoser12@users.noreply.github.com>
2021-08-24 15:28:41 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-values = [47, 95, 88, 73, 88, 84]
-sum(values)
-len(values)
-average = sum(values)/len(values)
-                         
-=======
 grades = [47, 95, 88, 73, 88, 84]
 total = sum(grades)
 count = len(grades
@@ -12,8 +5,3 @@ average = sum(grades)/len(grades):
 print('The average of the grades is:', average)
 print('The number of grades is:',count)
 print('The total of the grades is:',total)
-<<<<<<< HEAD
-
->>>>>>> e8e7060c7c7393ed0fb1ffad985fde38298de9cd
-=======
->>>>>>> e66253e017814ba717a095a934acaee987bc8ce7
