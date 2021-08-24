4c50c5abfed4a217715f083f8b0e11000e5da8c0
bmoser12 <89146471+bmoser12@users.noreply.github.com>
2021-08-24 15:42:07 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -5,7 +5,7 @@ average = sum(grades)/len(grades)
 print('The average of the grades is:', average)
 print('The number of grades is:',count)
 print('The total of the grades is:',total)
-sorted(grades)
+sorted(grades):
   if count %2==0:
     median1 = grades[count//2]
     median2 = grades[count//2-1]
